f58075d0a74d5530ee6574a54751911aa0a6d48b
fedotsoldier <mumikrol@gmail.com>
2018-08-25 14:21:15 +0300

    Added beautiful interface

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	text=models.TextField()
 	created_date=models.DateTimeField(
-					default=timezone.now ) # `django.utils.timezone.now` # timezone.now()
+					default=timezone.now ) # timezone.now()
 	published_date=models.DateTimeField(
 					blank=True, null=True)
 
